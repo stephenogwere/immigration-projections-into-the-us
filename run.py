0aa8f26498d089dd64004c3cf3334352790fb3f7
stephenogwere <>
2023-12-31 10:34:25 +0000

    Add requirements for relevant for deployment

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 # Import dependencies
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -28,7 +27,7 @@ def get_processed_immigrants_abroad_data():
         print("Data should be six numbers separated by commas")
         print("Example: 20,30,40,50,60\n")
         
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
         
         processed_immigrants_abroad_data = data_str.split(",")
         
